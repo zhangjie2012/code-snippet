c5b73de47569df62de46ab6ce2bdc1114d5c020b
jerryzhang <chinajiezhang@gmail.com>
2017-12-06 11:20:50 +0800

    delete decode

--- Python3/utils/db/kv_store.py
+++ Python3/utils/db/kv_store.py
@@ -5,6 +5,7 @@ import redis
 
 class KVStore:
     """KVStore Redis 实现版本
+
     redis 即拥有 memcached 的高效, 又提供了丰富的数据结构(string, set, list,
     hash 等), 目前只提供了 string 的接口, 针对其它数据结构的接口在需要时再提供
 
@@ -12,11 +13,23 @@ class KVStore:
     1. 所有的 key 的数据类型均为 string
     2. 已知 value 的类型, 选择合适的接口. 比如如果你想存储 int, 应使用 set_int,
        QRedis 所有接口都不处理类型转换带来的异常
-    3. 不是线程安全的
+
+    为什么不直接使用 redis.py ?
+    默认的 redis.py 存储的值都是 string, 使用时需要在业务中判断类型;
+    而我希望在使用过程中明确的知道获得的类型，少一步转换过程;
+
+    doc:
+    [1] https://redis.io/
+    [2] https://github.com/andymccurdy/redis-py
     """
 
     def __init__(self, host, port=6379, password=None):
-        self.rds = redis.StrictRedis(host=host, port=port, password=password)
+        self.rds = redis.StrictRedis(
+            host=host,
+            port=port,
+            password=password,
+            decode_responses=True  # 避免每次都做 decode
+        )
 
     # -------------------------------------------------------------------------
     # `KEY` 操作
@@ -44,7 +57,7 @@ class KVStore:
         """
         return self.rds.expire(name, sec)
 
-    # --------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # `STRING` 操作
     #
     # 1. 接口使用时应保证所有的 value 类型都是 string
@@ -56,7 +69,13 @@ class KVStore:
     #
     # 参数解释:
     # ex, px: 超时时间, ex 单位是秒, px: 单位是毫秒; 如果都为 None 表示永不超时.
-    # --------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    def set_str(self, name, value, ex=None, px=None):
+        self.rds.set(name, value, ex, px)
+
+    def get_str(self, name):
+        return self.rds.get(name)
+
     def set_int(self, name, int_value, ex=None, px=None):
         self.set_str(name, str(int_value), ex, px)
 
@@ -78,24 +97,16 @@ class KVStore:
         value = self.get_str(name)
         return json.loads(value) if value else value
 
-    def set_str(self, name, value, ex=None, px=None):
-        self.rds.set(name, value, ex, px)
-
-    def get_str(self, name):
-        value = self.rds.get(name)
-        return value.decode('utf-8') if value else value
-
     def incr(self, key):
         return self.rds.incr(key)
 
     def get_incr(self, key):
-        """默认的 get 有解包操作，对于自增字段没有办法 get"""
         value = self.rds.get(key)
-        return int(value.decode('utf-8')) if value else 0
+        return int(value) if value else 0
 
-    # --------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # 服务器信息(SERVER INFORMATION)
-    # --------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     def size(self):
         """连接 db 中的 key 数量"""
         return self.rds.dbsize()
@@ -103,3 +114,19 @@ class KVStore:
     def clear_all(self):
         """清除连接 db 的所有数据"""
         return self.rds.flushdb()
+
+
+if __name__ == '__main__':
+    kv_store = KVStore('localhost')
+
+    # kv_store.set_str('k1', 'v1')
+    # print(kv_store.get_str('k1'))
+
+    # kv_store.set_int('int', 1000)
+    # print(kv_store.get_int('int'))
+
+    # kv_store.set_float('float', 1.2344)
+    # print(kv_store.get_float('float'))
+
+    # print(kv_store.incr('incr'))
+    print(kv_store.get_incr('incr'))
